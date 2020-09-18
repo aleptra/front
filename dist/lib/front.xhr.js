170b3a754fc689b681db01c70f030540bfb04a7f
josef <gabrielsson@josef.st>
2020-09-18 16:34:55 +0200

    Experimental

--- dist/lib/front.xhr.js
+++ dist/lib/front.xhr.js
@@ -14,56 +14,6 @@ function include(el){
 	});
 }
 
-/**
- * @param {string} target - Target DOM element.
- * @param {string or object} response - Response from XHR.
- * @param {string} doa - Do the action with the target.
- */
-function doOnSuccess(target, response, doa) {
-
-	var obj = '';
-
-	if(response) {
-    try {
-			obj = JSON.parse(response)
-    }catch(e) {
-    	//console.log(e);
-    }
-	}
-
-console.log("%c Do On Success (XHR Response): Target="+target+", Type="+doa, "background: yellow; color: black");
-
-	hideLoader(target);
-
-	arr = getAttributes('onsuccess');
-
-	for (i = 0; i < arr.length; i++) {
-		var str = arr[i][0];
-		var firstchar = str.substring(0,1);
-
-		if (firstchar == "*") { //global call
-			str = str.substring(1); // remove char
-		}
-
- 		var values = str.split(") ");
-		var valueAction = values[1];
-
-		console.log(str);
-
-		var value = values[0].split(":");
-		var value1 = value[0].substr(1);
-		var value2 = value[1];
-
-		var object = getObject(obj, value1); //returns from xhr
-
-		if (object) {
-			if ((value2.length > 0 && value2 == object) || value2.length == 0)
-					setTimeout(valueAction, 1);
-		}
-
-	}
-}
-
 var xhrQuick = function() {
 
 	var request = new XMLHttpRequest();
@@ -95,7 +45,7 @@ var xhrQuick = function() {
 
 		request.onload = function() {
 			//console.log("%c API (GET): "+this.responseText, "background: green; color: white");
-    	callback(this.responseText);
+    		callback(this.responseText);
 		};
 
 		request.onerror = function() {
@@ -124,12 +74,6 @@ var xhrQuick = function() {
   }
 }
 
-/**
- * @param {string} path - Path to XHR Controller (see xhr.php).
- * @param {string or objects} data - If data is passed use client.post.
- * @param {string} el - Target DOM Element.
- * @param {string} doa - Pass arguments to DoOnSuccess function.
- */
 function xhr(path, data, el, doa) {
 
 	var doa = (doa) ? null : doa;
@@ -140,10 +84,7 @@ function xhr(path, data, el, doa) {
 		post = eval(xhrpost[0][0]);
 		data = [];
 		data.push(post);
-		//console.log("dataRaw", dom.value('q'));
 	}
-
-//console.log("dataRaw", dom.value('q'));
 
 	var url = '/?xhr='+path;
 	var code = 503;
