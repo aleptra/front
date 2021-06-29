b8601185411e5a4680009e8e121b5bb5024b0357
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2021-06-29 11:10:42 +0200

    .

--- dist/lib/globalize.js
+++ dist/lib/globalize.js
@@ -1,16 +1,16 @@
 libAttribute.push(
     {'attr': 'globalize', 'func': 'globalize'}
-);
+)
 
 libPreload.push(
     {'func': 'globalizePreload'}
-);
+)
 
-var trans = "";
-var htmlattr = dom.get("html?tag");
+var trans = "eng"
+var htmlattr = dom.get("html?tag")
 
 function globalizePreload(){
-    globalizeChangeLanguage(core.getParams()['lang']);
+    globalizeChangeLanguage(core.getParams()['lang'])
 }
 
 function globalizeChangeLanguage(q){
@@ -19,56 +19,56 @@ function globalizeChangeLanguage(q){
             if (core.isJson(response)) {
                 app.storage("lang", q)
                 app.storage(q, response)
-                var lang = app.storage("lang");
-                trans = app.storage(lang) ? JSON.parse(app.storage(lang)) : '';
-                globalizeChangeMetaDir(trans['direction']);
-                globalizeChangeMetaLanguage(q);
-                core.rerunLibAttributes("globalize");
+                var lang = app.storage("lang")
+                trans = app.storage(lang) ? JSON.parse(app.storage(lang)) : ''
+                globalizeChangeMetaDir(trans['direction'])
+                globalizeChangeMetaLanguage(q)
+                core.rerunLibAttributes("globalize")
             }
         },false);
     }else if (q == "") {
-        app.storage("lang", null);
+        app.storage("lang", null)
     }else if (app.storage("lang")) {
-        var lang = app.storage("lang");
-        trans = JSON.parse(app.storage(lang));
-        globalizeChangeMetaDir(trans['direction']);
+        var lang = app.storage("lang")
+        trans = JSON.parse(app.storage(lang))
+        globalizeChangeMetaDir(trans['direction'])
     }
 }
 
 function globalizeChangeMetaLanguage(lang){
-    htmlattr.lang = lang;
+    htmlattr.lang = lang
 }
 
 function globalizeChangeMetaDir(dir){
-    htmlattr.dir = dir;
-    htmlattr.classList.remove("ltr","rtl");
-    htmlattr.classList.add(dir);
+    htmlattr.dir = dir
+    htmlattr.classList.remove("ltr","rtl")
+    htmlattr.classList.add(dir)
 }
 
 function globalize(e){
     var value = e.getAttribute("globalize");
-    e.innerHTML = e.innerHTML.trim();
+    e.innerHTML = e.innerHTML.trim()
 
     if (trans['translations'] && trans['translations'][value]){
-        var children = e.childElementCount;
-        var name = e.localName;
-        var type = e.type;
+        var children = e.childElementCount
+        var name = e.localName
+        var type = e.type
         var globalized = trans['translations'][value];
         if (children > 0 && (name == "a" || name == "button" || name == "caption")) {
-            var child = e.firstChild;
+            var child = e.firstChild
             while (child) {
                 if (child.nodeType == 3) {
-                    child.data = globalized;
-                    break;
+                    child.data = globalized
+                    break
                 }
-                child = child.nextSibling;
+                child = child.nextSibling
             }
         }else if(name == "input" || type == "submit"){
-            e.value = globalized;
+            e.value = globalized
         }else if (name == "textarea" || name == "input"){
-            e.placeholder = globalized;
+            e.placeholder = globalized
         }else{
-            e.innerHTML = globalized;
+            e.innerHTML = globalized
         }
     }
 }
