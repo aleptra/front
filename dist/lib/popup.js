afceb4935cd48a05af8ccfdebb8e43cf97833dce
Josef Gabrielsson <josef.gabrielsson@gu.se>
2021-07-25 22:17:59 +0200

    .

--- dist/lib/popup.js
+++ dist/lib/popup.js
@@ -1,13 +1,13 @@
-var popup = function() {
+var popup = function(){
 
 	this.id = 'popup' // The name of the div that will be created
 
-	this.open = function(modal, data) {
+	this.open = function(modal, data){
 
 		var data = (data) ? data : ''
 
-		if (data) {
-			data.unshift(modal);
+		if(data){
+			data.unshift(modal)
 		}else{
 			data = [modal]
 		}
@@ -17,24 +17,24 @@ var popup = function() {
 		this.create()
 
     // This is temporary
-    if(dom.exists('menu')) {
-      dom.hide('menu');
+    if(dom.exists('menu')){
+      dom.hide('menu')
     }
     //
 
-		xhr('modal', data, 'xhrModal', [['content','xhrModal']]);
+		xhr('modal', data, 'xhrModal', [['content','xhrModal']])
     return false
 	}
 
-	this.create = function() {
+	this.create = function(){
 		dom.content(this.id, '<div id="popup-dialog" onclick="modal.close()"><div id="mypopup"><div id="popup-content" class="loaderb"><div class="m" onclick="window.event.stopPropagation()"><a onclick="modal.close()" class="close">Ã—</a><span id="xhrModal"></span></div></div></div></div>')
 	}
 
-	this.close = function() {
+	this.close = function(){
 		dom.hide(this.id)
 		dom.remove(this.id)
 	}
 }
 
 var popup = new popup(),
-    modal = popup;
+    modal = popup
--- src/js/lib/physics.js
+++ src/js/lib/physics.js
@@ -2,34 +2,34 @@ libAttribute.push(
   {'attr': 'slide', 'func': 'slide'}
 )
 
-var slideIndex = 1;
+var slideIndex = 1
 function slide(e){
 
-    params = e.getAttribute("slide").toObject();
-    console.dir(params);
-    //console.log(obj['domain']);
+  params = e.getAttribute("slide").toObject()
+  console.dir(params)
+  //console.log(obj['domain']);
 
-    var jdata = JSON.parse(JSON.stringify(e.getAttribute("slide").toString()))
-    console.dir(jdata)
+  var jdata = JSON.parse(JSON.stringify(e.getAttribute("slide").toString()))
+  console.dir(jdata)
 
-    showSlides(slideIndex)
+  showSlides(slideIndex)
 }
 
-function plusSlides(n) {
-    showSlides(slideIndex += n)
+function plusSlides(n){
+  showSlides(slideIndex += n)
 }
 
-function slideTo(n) {
-    showSlides(slideIndex = n)
+function slideTo(n){
+  showSlides(slideIndex = n)
 }
 
 function showSlides(n) {
-    var i
-    var slides = document.getElementsByClassName("slide")
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++){
-        slides[i].style.display = "none"
-    }
-    slides[slideIndex-1].style.display = "inherit"
+  var i
+  var slides = document.getElementsByClassName("slide")
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++){
+    slides[i].style.display = "none"
+  }
+  slides[slideIndex-1].style.display = "inherit"
 }
