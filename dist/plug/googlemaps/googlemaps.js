68f4b3be9622e4f1ecb67fb873520762b0e85216
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2021-07-27 19:08:47 +0200

    .

--- dist/plug/googlemaps/googlemaps.js
+++ dist/plug/googlemaps/googlemaps.js
@@ -1,17 +1,15 @@
 /*
     Author: Josef Gabrielsson
-    Version: 0.1.1
+    Version: 0.1.2
 */
 
 require("https://maps.googleapis.com/maps/api/js?key=AIzaSyAev1q4GNC-VlKYxWQXWfuPQ3pexINKOEY&callback=initMap&libraries=&v=weekly")
 
 function initMap(){
-  var el = dom.get("googlemaps")
-      attr = el.getAttribute("googlemaps").split(",")
-      latVal = attr[0].split(":")[1]
-      lngVal = attr[1].split(":")[1]
-
-  if (el)
+  if(dom.get("googlemaps")){
+    var attr = el.getAttribute("googlemaps").split(",")
+        latVal = attr[0].split(":")[1],
+        lngVal = attr[1].split(":")[1]
     var googleMapsOptions = {
       zoom: 8,
       center: { lat: parseFloat(latVal), lng: parseFloat(lngVal) },
@@ -19,4 +17,5 @@ function initMap(){
     }
 
     map = new google.maps.Map(el, googleMapsOptions)
+  }
 }
