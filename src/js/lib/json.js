451b291e9bed13f9a7ab49970849419d87414fcd
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2021-07-27 03:42:36 +0200

    .

--- src/js/lib/json.js
+++ src/js/lib/json.js
@@ -96,7 +96,6 @@ function json(aEl){
       eval(onempty)
     }
 
-    json = (iterate.indexOf('.')) ? json[0] : json
     json = (iterate === "true" || iterate === "false") ? json : eval("json."+iterate)
     el.innerHTML = xhr.el.innerHTML
 
