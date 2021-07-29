580935a0db9210b605b3335e34875724f0c05db1
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2021-07-29 18:40:09 +0200

    Experimental

--- dist/front.js
+++ dist/front.js
@@ -273,7 +273,7 @@ var core = function(){
 						  var response = this.responseText.match(/<template[^>]*>([\s\S]*?)<\/template>/gm)
 							getOneTemplate(response)
 					}
-					xhr.open("GET", url+"/"+template2+".html")
+					xhr.open("GET", url+"/"+template2+".html", false)
 					xhr.send()
 				}
 
@@ -287,7 +287,7 @@ var core = function(){
 							document.write(response)
 							document.close()
 					}
-					xhr.open("GET", url+"/"+template1+".html")
+					xhr.open("GET", url+"/"+template1+".html", false)
 					xhr.send()
 				}
 
