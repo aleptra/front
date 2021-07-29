06cf9bba52f4e9b9056eb3ef9ee5cd95aded5183
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2021-07-29 15:14:27 +0200

    .

--- dist/plug/googlemaps/googlemaps.js
+++ dist/plug/googlemaps/googlemaps.js
@@ -15,15 +15,15 @@ function googlemapsPreload(){
     var attr = el.getAttribute("googlemaps").split(",")
         latVal = attr[0].split(":")[1],
         lngVal = attr[1].split(":")[1]
-  }
 
-  var location = { lat: -25.344, lng: 131.036 }
-      map = new google.maps.Map(el,{
-        zoom: 4,
-        center: location,
-      }),
-    marker = new google.maps.Marker({
-        position: location,
-        map: map,
-      })
+    var location = { lat: -25.344, lng: 131.036 }
+    var map = new google.maps.Map(el,{
+          zoom: 4,
+          center: location,
+        })
+    var marker = new google.maps.Marker({
+          position: location,
+          map: map,
+        })
+  }
 }
