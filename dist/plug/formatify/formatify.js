3d7a7802816585a6190bed3338c051c8d613353b
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2021-07-30 20:58:35 +0200

    .

--- dist/plug/formatify/formatify.js
+++ dist/plug/formatify/formatify.js
@@ -12,23 +12,23 @@ libAttribute.push(
 )
 
 function formatify(el){
-  var attr = el.getAttribute("formatify").split(";");
-  var target = el;
-  el = _.unescape(target.innerHTML.trim());
+  var attr = el.getAttribute("formatify").split(";")
+  var target = el
+  el = _.unescape(target.innerHTML.trim())
 
   var tab = "\t",
       result = "",
       indent = ""
 
   el.split(/>\s+<[^>\s<\/]{0}/).forEach(function(element){
-      if(element.match(/^\/\w/)){
-          indent = indent.substring(tab.length);
-      }
+    if(element.match(/^\/\w/)){
+      indent = indent.substring(tab.length)
+    }
 
-      result += indent + "<" + element + ">\r\n";
+    result += indent + "<" + element + ">\r\n";
       if (element.match(/^<?\w[^>]*[^\/]$/) && !element.match(/^doctype|meta|base|link|input|img|hr.*$/i))
-          indent += tab;
-    })
+          indent += tab
+  })
 
   result = _.escape(result.substring(1, result.length-3))
   if (attr[1]) result = result.replace(/&#0009;/ig, "&emsp;").replace(/&#0010;/ig, "<br>")
@@ -37,41 +37,41 @@ function formatify(el){
 
 function formatify_colorize(text, attr){
 
-    var color = []
+  var color = []
 
-    for(val in attr){
-        var prop = attr[val].split(":")
-        if (prop[0] == "colors")
-            color = prop[1].split(",")
-    }
+  for(val in attr){
+    var prop = attr[val].split(":")
+    if (prop[0] == "colors")
+      color = prop[1].split(",")
+  }
 
-    //Name
-    text = text.replace(/(\w+&#0062;|&#0060;\w+|&#0060;&#0047;\w+)/ig, function(x){
-        var pos = (x.substr(0, 14) == "&#0060;&#0047;") ? 14 : 7
-        var x = dom.insertAt(x, pos, '<span class="'+color[0]+'">')
-        return x+'</span>'
-    });
+  //Name
+  text = text.replace(/(\w+&#0062;|&#0060;\w+|&#0060;&#0047;\w+)/ig, function(x){
+    var pos = (x.substr(0, 14) == "&#0060;&#0047;") ? 14 : 7
+    var x = dom.insertAt(x, pos, '<span class="'+color[0]+'">')
+    return x+'</span>'
+  })
 
-    //Attribute
-    text = text.replace(/[\w]+&#0061;/ig, function(x){
-            var a = dom.insertAt(x, (x.length - 7), "</span>")
-            return '<span class="'+color[1]+'">'+a
-    })
+  //Attribute
+  text = text.replace(/[\w]+&#0061;/ig, function(x){
+    var a = dom.insertAt(x, (x.length - 7), "</span>")
+    return '<span class="'+color[1]+'">'+a
+  })
 
-    //Value
-    var text = text.replace(/&#0034;(.*?)&#0034;/ig, function(x){
-        return '<span class="'+color[2]+'">'+x+'</span>'
-    })
+  //Value
+  var text = text.replace(/&#0034;(.*?)&#0034;/ig, function(x){
+    return '<span class="'+color[2]+'">'+x+'</span>'
+  })
 
-    //Bracket
-    text = text.replace(/(&#0060;&#0047;|&#0060;|&#0062;)/ig, function(x){
-        return '<span class="'+color[3]+'">'+x+'</span>'
-    })
+  //Bracket
+  text = text.replace(/(&#0060;&#0047;|&#0060;|&#0062;)/ig, function(x){
+    return '<span class="'+color[3]+'">'+x+'</span>'
+  })
 
-    //Comment
-    text = text.replace(/<\w+\s+\S+&#0033;&#0045;&#0045;.(.*?).&#0045;&#0045;<\w+\s+\S+>/ig, function(x,y){
-        return '<span class="'+color[4]+'">'+dom.removeAllTags(x)+'</span>'
-    })
+  //Comment
+  text = text.replace(/<\w+\s+\S+&#0033;&#0045;&#0045;.(.*?).&#0045;&#0045;<\w+\s+\S+>/ig, function(x,y){
+    return '<span class="'+color[4]+'">'+dom.removeAllTags(x)+'</span>'
+  })
 
-    return text
+  return text
 }
