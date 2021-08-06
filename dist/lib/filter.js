09163c8f4a6e186888751515b576dd47f6aa859b
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2021-08-06 16:53:39 +0200

    .

--- dist/lib/filter.js
+++ dist/lib/filter.js
@@ -12,21 +12,14 @@ function filter(){
   var e = event && (event.target || event.srcElement)
   var filter = e.value.toUpperCase()
 
-  var children = _.getChildren(target)
-
-  for(i = 0; i < children.length; i++){
-
-    if(children[i].childElementCount > 0){
-      var child = _.getChildren(children[i])
-
-      for(j = 0; j < child.length; j++){
-        if(child[j].hasAttribute("filteron")){
-          if (child[j].textContent.toUpperCase().indexOf(filter) > -1 || child[j - 1].textContent.toUpperCase().indexOf(filter) > -1)
-            children[i].style.display = ""
-          else
-            children[i].style.display = "none"
-        }
+  var children = target.getElementsByTagName('*');
+  for(var i = 0; i < children.length; i++){
+      if(children[i].hasAttribute("filteron")){
+        var filteron = children[i].getAttribute("filteron")
+        if (filteron.toUpperCase().indexOf(filter) > -1)
+          children[i].style.display = ""
+        else
+          children[i].style.display = "none"
       }
-    }
   }
 }
