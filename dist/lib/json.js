ad6e7fd4111ccdc20e88bfdef56f0154ce017807
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2021-08-13 16:13:21 +0200

    .

--- dist/lib/json.js
+++ dist/lib/json.js
@@ -238,10 +238,6 @@ function jsonPush(payload, e){
   var xhr = new XMLHttpRequest()
   xhr.open(method, url, true)
   xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
-  xhr.setRequestHeader("Access-Control-Allow-Origin", "*")
-  xhr.setRequestHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  xhr.setRequestHeader("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS,PATCH")
-  xhr.setRequestHeader("Accept", "application/json")
   xhr.onreadystatechange = function(){
     console.log(this.status)
     if(this.readyState == 4 && (this.status == 200 || this.status == 201 || this.status == 204))
