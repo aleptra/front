b72b9a4f3837cf7983b3604e653acf5cf427dfbe
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2025-06-15 22:26:53 +0200

    Build 282

--- nightly/front.js
+++ nightly/front.js
@@ -1127,7 +1127,7 @@ var dom = {
 }
 
 var app = {
-  version: { major: 1, minor: 0, patch: 0, build: 277 },
+  version: { major: 1, minor: 0, patch: 0, build: 281 },
   module: {},
   plugin: {},
   var: {},
