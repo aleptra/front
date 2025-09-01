80e5a18deed4da4daf8becfdac5023ee4e52dcbe
aramean <gabrielsson@josef.st>
2025-09-01 17:07:52 +0200

    Build 288

--- nightly/front.js
+++ nightly/front.js
@@ -2400,19 +2400,17 @@ var app = {
           responsePageContent = responsePage.innerHTML,
           responsePageBodyAttr = responsePage.attrList
 
-        /*for (var i = 0; i < this.elementSelectors.length; i++) {
+        for (var i = 0; i < this.elementSelectors.length; i++) {
           var elSelector = this.elementSelectors[i],
             parsedEl = app.element.find(responsePage, elSelector.name),
             content = parsedEl.innerHTML
-
-          console.log(elSelector.name)
 
           if (elSelector.name !== 'main') {
             elSelector.content = content
             dom.set(elSelector.name, content ? content : '')
             app.attributes.run(elSelector.name + ' *')
           }
-        }*/
+        }
 
         if (!isReload) {
           app.assets.set(responsePageScript.attributes)
