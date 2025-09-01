07602f3c57c5e2559abe5883ab2077747d47b60d
aramean <gabrielsson@josef.st>
2025-09-01 23:07:24 +0200

    Build 288

--- nightly/modules/screen.js
+++ nightly/modules/screen.js
@@ -3,100 +3,111 @@
 app.module.screen = {
   __autoload: function (options) {
     this.module = options.name
-    this.currentBp = null
-    this._resizeTimer = null
-
-    this.updateDimensions()
     var self = this
-    var onResize = function () {
-      self.updateDimensions()
+
+    // initialize globals
+    app.globals.windowHeight = window.innerHeight
+    app.globals.windowWidth = window.innerWidth
+
+    // keep track of active breakpoint
+    this.currentBp = null
+
+    // re-check on resize
+    app.listeners.add(window, 'onload', function () {
       self.checkBreakpoints()
-    }
+    })
 
-    var debounced = function () {
-      clearTimeout(self._resizeTimer)
-      self._resizeTimer = setTimeout(onResize, 100)
-    }
-
-    if (window.visualViewport) {
-      window.visualViewport.addEventListener('resize', debounced)
-      window.visualViewport.addEventListener('scroll', debounced)
-    }
-    window.addEventListener('resize', debounced)
-    window.addEventListener('orientationchange', debounced)
+    // re-check on resize
+    app.listeners.add(window, 'resize', function () {
+      app.globals.windowHeight = window.innerHeight
+      app.globals.windowWidth = window.innerWidth
+      self.checkBreakpoints()
+    })
   },
 
+  // define breakpoint ranges
   breakpoints: {
-    xs: [0, 399], sm: [400, 575], md: [576, 767],
-    lg: [768, 991], xl: [992, 1199], xxl: [1200, Infinity]
+    xs: [0, 399],
+    sm: [400, 575],
+    md: [576, 767],
+    lg: [768, 991],
+    xl: [992, 1199],
+    xxl: [1200, Infinity]
   },
 
+  // ordered list of breakpoints (for fallback search)
   bpOrder: ['xs', 'sm', 'md', 'lg', 'xl', 'xxl'],
 
-  updateDimensions: function () {
-    var vv = window.visualViewport || null
-    app.globals.windowHeight = (vv && vv.height) || window.innerHeight
-    app.globals.windowWidth = (vv && vv.width) || window.innerWidth
-  },
-
-  getCurrentBreakpoint: function () {
-    var w = app.globals.windowWidth
-    var order = this.bpOrder
-    for (var i = 0; i < order.length; i++) {
-      var k = order[i]
-      var r = this.breakpoints[k]
-      if (w >= r[0] && w <= r[1]) return k
-    }
-    return null
-  },
-
   checkBreakpoints: function () {
-    var bp = this.getCurrentBreakpoint()
+    var w = app.globals.windowWidth
+    var bp = null
+
+    // find matching breakpoint
+    for (var key in this.breakpoints) {
+      var range = this.breakpoints[key]
+      if (w >= range[0] && w <= range[1]) {
+        bp = key
+        break
+      }
+    }
+
+    // fire event only if breakpoint changed
     if (bp && bp !== this.currentBp) {
       this.currentBp = bp
       this.applyForBreakpoint(bp)
     }
   },
 
+  // finds closest available attribute for each element (fallback logic)
   applyForBreakpoint: function (bp) {
-    var order = this.bpOrder
-    var attrPrefix = this.module + '-'
-    var selParts = []
-    for (var i = 0; i < order.length; i++) {
-      selParts.push('[' + attrPrefix + order[i] + ']')
+    var selector = ''
+    for (var i = 0; i < this.bpOrder.length; i++) {
+      selector += (i > 0 ? ',' : '') + '[' + this.module + '-' + this.bpOrder[i] + ']'
     }
-    var els = dom.get(selParts.join(','), true) || []
-    for (var j = 0; j < els.length; j++) {
-      var act = this.findClosestAttr(els[j], bp)
-      if (act) app.call(act, { element: els[j] })
+
+    var elements = dom.get(selector, true)
+    for (var i = 0; i < elements.length; i++) {
+      var el = elements[i]
+      var action = this.findClosestAttr(el, bp)
+      if (action) {
+        app.call(action, { element: el })
+      }
     }
   },
 
+  // search fallback: current bp → smaller ones
   findClosestAttr: function (el, bp) {
     var idx = this.bpOrder.indexOf(bp)
-    var prefix = this.module + '-'
     for (var i = idx; i >= 0; i--) {
-      var name = prefix + this.bpOrder[i]
-      var val = el.getAttribute && el.getAttribute(name)
-      if (val) return val
+      var attrName = this.module + '-' + this.bpOrder[i]
+      var value = el.getAttribute(attrName)
+      if (value) return value
     }
     return null
   },
 
-  _initRun: function (el) {
-    var bp = this.getCurrentBreakpoint()
-    if (!bp) return
-    var val = this.findClosestAttr(el, bp)
-    if (val) {
-      this.currentBp = bp
-      app.call(val, { element: el })
+  _initRun: function (attr, element) {
+    var w = app.globals.windowWidth
+    var bp = null
+    for (var key in this.breakpoints) {
+      var range = this.breakpoints[key]
+      if (w >= range[0] && w <= range[1]) {
+        bp = key
+        break
+      }
     }
-  }
-}
 
-// generate xs(), sm(), ... methods
-for (var i = 0; i < app.module.screen.bpOrder.length; i++) {
-  (function (bp) {
-    app.module.screen[bp] = function (el) { this._initRun(el) }
-  })(app.module.screen.bpOrder[i])
+    var value = this.findClosestAttr(element, bp)
+    if (value) {
+      this.currentBp = bp
+      app.call(value, { element: element })
+    }
+  },
+
+  xs: function (element) { this._initRun('xs', element) },
+  sm: function (element) { this._initRun('sm', element) },
+  md: function (element) { this._initRun('md', element) },
+  lg: function (element) { this._initRun('lg', element) },
+  xl: function (element) { this._initRun('xl', element) },
+  xxl: function (element) { this._initRun('xxl', element) }
 }
