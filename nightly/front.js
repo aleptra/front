8b6b48d322200438a7499f358c8e8a4c9874cde0
aramean <gabrielsson@josef.st>
2025-09-21 14:08:02 +0200

    Build 304

--- nightly/front.js
+++ nightly/front.js
@@ -1192,7 +1192,7 @@ var dom = {
 }
 
 var app = {
-  version: { major: 1, minor: 0, patch: 0, build: 303 },
+  version: { major: 1, minor: 0, patch: 0, build: 304 },
   module: {},
   plugin: {},
   var: {},
