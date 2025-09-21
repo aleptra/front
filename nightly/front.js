cb9068ffcfbba5069043412d219dcdd9ef59d6e6
aramean <gabrielsson@josef.st>
2025-09-21 13:28:58 +0200

    Build 297

--- nightly/front.js
+++ nightly/front.js
@@ -1192,7 +1192,7 @@ var dom = {
 }
 
 var app = {
-  version: { major: 1, minor: 0, patch: 0, build: 296 },
+  version: { major: 1, minor: 0, patch: 0, build: 297 },
   module: {},
   plugin: {},
   var: {},
