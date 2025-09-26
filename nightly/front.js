3f1b5441e4d8aa3d5fa7838a231ace32fe42f774
Josef Gabrielsson <gabrielsson@josef.st>
2025-09-26 07:29:48 +0200

    Build 322

--- nightly/front.js
+++ nightly/front.js
@@ -1192,7 +1192,7 @@ var dom = {
 }
 
 var app = {
-  version: { major: 1, minor: 0, patch: 0, build: 321 },
+  version: { major: 1, minor: 0, patch: 0, build: 322 },
   module: {},
   plugin: {},
   var: {},
