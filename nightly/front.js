7f3b9885c6d4c018d9513c169887e9e074dbd112
Josef Gabrielsson <gabrielsson@josef.st>
2025-10-26 13:36:12 +0100

    Build 374

--- nightly/front.js
+++ nightly/front.js
@@ -1241,7 +1241,7 @@ var dom = {
 }
 
 var app = {
-  version: { major: 1, minor: 0, patch: 0, build: 373 },
+  version: { major: 1, minor: 0, patch: 0, build: 374 },
   module: {},
   plugin: {},
   var: {},
