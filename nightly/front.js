db10f03356359b5df50ae39006d44e1918c94db0
Josef Gabrielsson <gabrielsson@josef.st>
2025-10-30 10:07:01 +0100

    Build 392

--- nightly/front.js
+++ nightly/front.js
@@ -1220,7 +1220,7 @@ var dom = {
 }
 
 var app = {
-  version: { major: 1, minor: 0, patch: 0, build: 391 },
+  version: { major: 1, minor: 0, patch: 0, build: 392 },
   module: {},
   plugin: {},
   var: {},
