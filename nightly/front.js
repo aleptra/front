2a5ba1b08f138c97d32bd2a22f77b5104c41e214
Josef Gabrielsson <gabrielsson@josef.st>
2025-11-06 14:34:34 +0100

    Build 409

--- nightly/front.js
+++ nightly/front.js
@@ -1228,7 +1228,7 @@ var dom = {
 }
 
 var app = {
-  version: { major: 1, minor: 0, patch: 0, build: 408 },
+  version: { major: 1, minor: 0, patch: 0, build: 409 },
   module: {},
   plugin: {},
   var: {},
