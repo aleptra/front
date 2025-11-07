45420408bb2ddd0558ab222506023d63c28c4a49
Josef Gabrielsson <gabrielsson@josef.st>
2025-11-07 21:01:58 +0100

    Build 413

--- nightly/front.js
+++ nightly/front.js
@@ -1234,7 +1234,7 @@ var dom = {
 }
 
 var app = {
-  version: { major: 1, minor: 0, patch: 0, build: 412 },
+  version: { major: 1, minor: 0, patch: 0, build: 413 },
   module: {},
   plugin: {},
   var: {},
