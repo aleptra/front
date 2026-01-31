83f1939bcf70958cdccd92ed0833c9b7129aa401
Josef Gabrielsson <gabrielsson@josef.st>
2026-01-31 15:59:54 +0100

    Build 534

--- nightly/front.js
+++ nightly/front.js
@@ -1421,7 +1421,6 @@ var dom = {
 }
 
 var app = {
-  version: { major: 1, minor: 0, patch: 0, build: 533 },
   module: {},
   plugin: {},
   var: {},
@@ -2375,6 +2374,7 @@ var app = {
    * @desc Handles global variables for the application.
    */
   globals: {
+    frontVersion: { major: 1, minor: 0, patch: 0, build: 534 },
     language: document.documentElement.lang || 'en',
     docMode: document.documentMode || 0,
     isFrontpage: document.doctype ? true : false,
