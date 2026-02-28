d10615734dffdf2a60966b781c1f43506b0ee6b5
Josef Gabrielsson <gabrielsson@josef.st>
2026-02-28 14:17:52 +0100

    Build 574

--- nightly/front.js
+++ nightly/front.js
@@ -2427,7 +2427,7 @@ var app = {
    * @desc Handles global variables for the application.
    */
   globals: {
-    frontVersion: { major: 1, minor: 0, patch: 0, build: 573 },
+    frontVersion: { major: 1, minor: 0, patch: 0, build: 574 },
     language: document.documentElement.lang || 'en',
     docMode: document.documentMode || 0,
     isFrontpage: document.doctype ? true : false,
--- nightly/modules/screen.js
+++ nightly/modules/screen.js
@@ -5,34 +5,30 @@ app.module.screen = {
     this.module = options.name
     var self = this
 
-    // initialize globals
     app.globals.windowHeight = window.innerHeight
     app.globals.windowWidth = window.innerWidth
-
-    // keep track of active breakpoint
     this.currentBp = null
 
-    function updateViewport() {
+    this._updateCurrentBp()
+
+    // Function to run directly on events without debounce
+    var handleUpdate = function () {
       var vv = window.visualViewport || null
-      var h = (vv && vv.height) || window.innerHeight
-      var w = (vv && vv.width) || window.innerWidth
-      app.globals.windowHeight = h
-      app.globals.windowWidth = w
-      self.checkBreakpoints()
+      app.globals.windowHeight = (vv && vv.height) || window.innerHeight
+      app.globals.windowWidth = (vv && vv.width) || window.innerWidth
+      self._checkBreakpoints()
     }
 
-    app.listeners.add(window, 'orientationchange', updateViewport)
-    app.listeners.add(window, 'resize', updateViewport)
+    app.listeners.add(window, 'orientationchange', handleUpdate)
+    app.listeners.add(window, 'resize', handleUpdate)
 
-    // visualViewport is best on mobile
     if (window.visualViewport) {
-      window.visualViewport.addEventListener('resize', updateViewport)
-      window.visualViewport.addEventListener('scroll', updateViewport)
+      window.visualViewport.addEventListener('resize', handleUpdate)
+      window.visualViewport.addEventListener('scroll', handleUpdate)
     }
   },
 
-  // define breakpoint ranges
-  breakpoints: {
+  _breakpoints: {
     xs: [0, 399],
     sm: [400, 575],
     md: [576, 767],
@@ -43,88 +39,82 @@ app.module.screen = {
     wide: [1920, 2559],
     ultra: [2560, 3439],
     cinema: [3440, 3999],
-    mega: [4000, 9999],
+    mega: [4000, 9999]
   },
 
-  // ordered list of breakpoints (for fallback search)
-  bpOrder: ['xs', 'sm', 'md', 'lg', 'xl', 'xxl', 'xxxl', 'wide', 'ultra', 'cinema', 'mega'],
+  _bpOrder: ['xs', 'sm', 'md', 'lg', 'xl', 'xxl', 'xxxl', 'wide', 'ultra', 'cinema', 'mega'],
 
-  checkBreakpoints: function () {
-    var w = app.globals.windowWidth
-    var bp = null
-
-    // find matching breakpoint
-    for (var key in this.breakpoints) {
-      var range = this.breakpoints[key]
-      if (w >= range[0] && w <= range[1]) {
-        bp = key
-        break
-      }
+  _getBpByWidth: function (w) {
+    var keys = this._bpOrder
+    for (var i = 0; i < keys.length; i++) {
+      var range = this._breakpoints[keys[i]]
+      if (w >= range[0] && w <= range[1]) return keys[i]
     }
+    return null
+  },
 
-    // fire event only if breakpoint changed
+  _updateCurrentBp: function () {
+    this.currentBp = this._getBpByWidth(app.globals.windowWidth)
+  },
+
+  _checkBreakpoints: function () {
+    var w = app.globals.windowWidth,
+      bp = this._getBpByWidth(w)
+
     if (bp && bp !== this.currentBp) {
       this.currentBp = bp
-      app.log.info()('Current breakpoint:', bp, 'Window width:', w)
-      this.applyForBreakpoint(bp)
+      this._applyForBreakpoint(bp)
     }
   },
 
-  // finds closest available attribute for each element (fallback logic)
-  applyForBreakpoint: function (bp) {
-    var selector = ''
-    for (var i = 0; i < this.bpOrder.length; i++) {
-      selector += (i > 0 ? ',' : '') + '[' + this.module + '-' + this.bpOrder[i] + ']'
-    }
+  _applyForBreakpoint: function (bp) {
+    var prefix = this.module + '-',
+      selector = '[' + prefix + this._bpOrder.join('],[' + prefix) + ']',
+      elements = app.element.select(selector, true)
 
-    var elements = app.element.select(selector, true)
     for (var i = 0; i < elements.length; i++) {
-      var el = elements[i]
-      var action = this.findClosestAttr(el, bp)
-      if (action) {
-        app.call(action, { element: el })
-      }
+      this._runElementLogic(elements[i], bp)
     }
   },
 
-  // search fallback: current bp → smaller ones
-  findClosestAttr: function (el, bp) {
-    var idx = this.bpOrder.indexOf(bp)
+  // Centralized logic for evaluating an element
+  _runElementLogic: function (el, bp) {
+    var action = this._findClosestAttr(el, bp)
+    if (action) app.call(action, { element: el })
+  },
+
+  _findClosestAttr: function (el, bp) {
+    var idx = this._bpOrder.indexOf(bp)
     for (var i = idx; i >= 0; i--) {
-      var attrName = this.module + '-' + this.bpOrder[i]
-      var value = el.getAttribute(attrName)
+      var attrName = this.module + '-' + this._bpOrder[i],
+        value = el.getAttribute(attrName)
       if (value) return value
     }
     return null
   },
 
-  _initRun: function (attr, element) {
-    var w = app.globals.windowWidth
-    var bp = null
-    for (var key in this.breakpoints) {
-      var range = this.breakpoints[key]
-      if (w >= range[0] && w <= range[1]) {
-        bp = key
-        break
-      }
-    }
+  // Shared entry point for all attribute triggers
+  _initRun: function (triggerBp, element) {
+    if (!this.currentBp) this._updateCurrentBp()
 
-    var value = this.findClosestAttr(element, bp)
-    if (value) {
-      this.currentBp = bp
-      app.call(value, { element: element })
+    var winningValue = this._findClosestAttr(element, this.currentBp),
+      triggerValue = element.getAttribute(this.module + '-' + triggerBp)
+
+    // Only run if the attribute being initialized is the one that SHOULD be active
+    if (winningValue && winningValue === triggerValue) {
+      this._runElementLogic(element, this.currentBp)
     }
   },
 
-  xs: function (element) { this._initRun('xs', element) },
-  sm: function (element) { this._initRun('sm', element) },
-  md: function (element) { this._initRun('md', element) },
-  lg: function (element) { this._initRun('lg', element) },
-  xl: function (element) { this._initRun('xl', element) },
-  xxl: function (element) { this._initRun('xxl', element) },
-  xxxl: function (element) { this._initRun('xxxl', element) },
-  wide: function (element) { this._initRun('wide', element) },
-  ultra: function (element) { this._initRun('ultra', element) },
-  cinema: function (element) { this._initRun('cinema', element) },
-  mega: function (element) { this._initRun('mega', element) }
+  xs: function (el) { this._initRun('xs', el) },
+  sm: function (el) { this._initRun('sm', el) },
+  md: function (el) { this._initRun('md', el) },
+  lg: function (el) { this._initRun('lg', el) },
+  xl: function (el) { this._initRun('xl', el) },
+  xxl: function (el) { this._initRun('xxl', el) },
+  xxxl: function (el) { this._initRun('xxxl', el) },
+  wide: function (el) { this._initRun('wide', el) },
+  ultra: function (el) { this._initRun('ultra', el) },
+  cinema: function (el) { this._initRun('cinema', el) },
+  mega: function (el) { this._initRun('mega', el) }
 }
