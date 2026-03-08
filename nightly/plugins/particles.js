d203fe5606be1a608a020577aa8a0f556c2cd791
Josef Gabrielsson <gabrielsson@josef.st>
2026-03-08 15:13:25 +0100

    Build 580

--- nightly/plugins/particles.js
+++ nightly/plugins/particles.js
@@ -2,82 +2,94 @@
 
 app.plugin.particles = {
   config: {
-    starCount: 50,   // total stars on screen
+    particles: 50,   // total particles
     maxSpeed: 1,     // vertical speed
     size: 2,
-    colors: ['#fff', '#ff0', '#0ff', '#f0f']
+    colors: ['#fff', '#ff0', '#0ff', '#f0f'],
+    mousemove: true  // enable/disable mouse tracking
   },
 
-  load: function(container) {
+  __autoload: function (options) {
+    var conf = app.config.get('particles-', this.config, options.element)
+    for (var key in conf) {
+      if (conf.hasOwnProperty(key)) this.config[key] = conf[key]
+    }
+  },
+
+  load: function (container) {
     var self = this
 
     // Ensure container has proper styles
     container.style.position = 'relative'
     container.style.overflow = 'hidden'
 
-    var stars = []
+    var particles = []
     var mouseX = container.offsetWidth / 2
     var mouseY = container.offsetHeight / 2
 
-    // Create stars randomly inside the container
-    for (var i = 0; i < self.config.starCount; i++) {
-      createStar(Math.random() * container.offsetWidth, Math.random() * container.offsetHeight)
+    // Create particles randomly inside the container
+    for (var i = 0; i < self.config.particles; i++) {
+      createParticle(Math.random() * container.offsetWidth, Math.random() * container.offsetHeight)
     }
 
-    function createStar(x, y) {
-      var star = document.createElement('div')
+    function createParticle(x, y) {
+      var particle = document.createElement('div')
 
-      star.style.position = 'absolute';
-      star.style.width = self.config.size + 'px'
-      star.style.height = self.config.size + 'px'
+      particle.style.position = 'absolute'
+      particle.style.width = self.config.size + 'px'
+      particle.style.height = self.config.size + 'px'
       var colorIndex = Math.floor(Math.random() * self.config.colors.length)
-      star.style.backgroundColor = self.config.colors[colorIndex]
-      star.style.borderRadius = '50%'
-      star.style.pointerEvents = 'none'
-      star.style.opacity = 0.4
-      star.style.left = x + 'px'
-      star.style.top = y + 'px'
+      particle.style.backgroundColor = self.config.colors[colorIndex]
+      particle.style.borderRadius = '50%'
+      particle.style.pointerEvents = 'none'
+      particle.style.opacity = 0.4
+      particle.style.left = x + 'px'
+      particle.style.top = y + 'px'
 
       // Random vertical speed
-      star.speedY = Math.random() * self.config.maxSpeed + 0.5
-      star.speedX = 0 // initially no horizontal speed
+      particle.speedY = Math.random() * self.config.maxSpeed + 0.5
+      particle.speedX = 0 // initially no horizontal speed
 
-      container.appendChild(star)
-      stars.push(star)
+      container.appendChild(particle)
+      particles.push(particle)
     }
 
-    function animateStars() {
-      for (var i = 0; i < stars.length; i++) {
-        var star = stars[i]
-        var top = parseFloat(star.style.top)
-        var left = parseFloat(star.style.left)
+    function animateParticles() {
+      for (var i = 0; i < particles.length; i++) {
+        var particle = particles[i]
+        var top = parseFloat(particle.style.top)
+        var left = parseFloat(particle.style.left)
 
         // Move down
-        top += star.speedY
+        top += particle.speedY
 
-        // Move slightly toward mouse
-        var dx = (mouseX - left) * 0.002 // small factor
-        left += dx
+        // Move slightly toward mouse (if enabled)
+        if (self.config.mousemove !== false && self.config.mousemove !== 'false') {
+          var dx = (mouseX - left) * 0.002 // small factor
+          left += dx
+        }
 
-        star.style.top = top + 'px'
-        star.style.left = left + 'px'
+        particle.style.top = top + 'px'
+        particle.style.left = left + 'px'
 
         // Wrap around vertically
         if (top > container.offsetHeight) {
-          star.style.top = '0px'
-          star.style.left = Math.random() * container.offsetWidth + 'px'
+          particle.style.top = '0px'
+          particle.style.left = Math.random() * container.offsetWidth + 'px'
         }
       }
 
-      requestAnimationFrame(animateStars)
+      requestAnimationFrame(animateParticles)
     }
 
-    app.listeners.add(container, 'mousemove', function(e) {
-      var rect = container.getBoundingClientRect()
-      mouseX = e.clientX - rect.left
-      mouseY = e.clientY - rect.top
-    })
+    if (self.config.mousemove !== false && self.config.mousemove !== 'false') {
+      app.listeners.add(container, 'mousemove', function (e) {
+        var rect = container.getBoundingClientRect()
+        mouseX = e.clientX - rect.left
+        mouseY = e.clientY - rect.top
+      })
+    }
 
-    animateStars()
+    animateParticles()
   }
 }
