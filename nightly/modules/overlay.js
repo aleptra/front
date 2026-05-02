10e3e397c7388ea98e16b48f44936c1fdb1b53e4
Josef Gabrielsson <gabrielsson@josef.st>
2026-05-02 22:50:32 +0200

    Build 624

--- nightly/modules/overlay.js
+++ nightly/modules/overlay.js
@@ -2,8 +2,23 @@
 
 app.module.overlay = {
   dialog: function (object) {
-    var dialog = object.exec.element
+    var dialog = object.exec.element,
+      srcEl = object.options && object.options.srcElement
+
     dialog.innerHTML = dialog.originalHtml
+
+    // Pass overlay-bind value from source element to dialog input with matching overlay-bind.
+    if (srcEl) {
+      var srcBind = srcEl.getAttribute('overlay-bind')
+      if (srcBind) {
+        var inputs = dialog.querySelectorAll('[overlay-bind]')
+        for (var i = 0; i < inputs.length; i++) {
+          inputs[i].value = srcBind
+          inputs[i].setAttribute('value', srcBind)
+        }
+      }
+    }
+
     dialog.showModal()
     dom.show(object)
   },
