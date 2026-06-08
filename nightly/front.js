3dfe1051e0345b5435f20799a4e9395a0e7ae45d
Josef Gabrielsson <gabrielsson@josef.st>
2026-06-08 16:26:21 +0200

    Build 652

--- nightly/front.js
+++ nightly/front.js
@@ -368,6 +368,18 @@ var dom = {
       case 'gridarea':
         attr = 'gridArea'
         break
+      case 'gridcolumns':
+        attr = 'gridTemplateColumns'
+        break
+      case 'gridrows':
+        attr = 'gridTemplateRows'
+        break
+      case 'gridcolumn':
+        attr = 'gridColumn'
+        break
+      case 'gridrow':
+        attr = 'gridRow'
+        break
       case 'inline':
         value = 'inline'
         attr = 'display'
@@ -2570,7 +2582,7 @@ var app = {
    * @desc Handles global variables for the application.
    */
   globals: {
-    frontVersion: { major: 1, minor: 0, patch: 0, build: 651 },
+    frontVersion: { major: 1, minor: 0, patch: 0, build: 652 },
     language: document.documentElement.lang || 'en',
     docMode: document.documentMode || 0,
     isFrontpage: document.doctype ? true : false,
