e9eab1c5cb6c54f466ea5f755f293a26fa8a7651
Josef Gabrielsson <gabrielsson@josef.st>
2026-06-26 21:14:36 +0200

    Build 674

--- nightly/plugins/syntaxhighlighting.js
+++ nightly/plugins/syntaxhighlighting.js
@@ -11,6 +11,7 @@ app.plugin.syntaxhighlighting = {
         javascriptColors: 'cornflowerblue,burlywood,green,mediumpurple,gray',
         shellColors: 'slategray,silver,cornsilk,navajowhite,green',
         errorColor: 'red',
+        indent: '  ',
       },
       options.element
     )
@@ -30,58 +31,111 @@ app.plugin.syntaxhighlighting = {
       object.innerHTML = this._colorizeJavaScript(object.innerHTML, this.config.javascriptColors || this.config.colors)
     }
 
-    if (object.isContentEditable && !object._syntaxBound) {
-      object._syntaxBound = true
-      var timer = null
-      object.addEventListener('input', function () {
-        clearTimeout(timer)
-        timer = setTimeout(function () {
-          var sel = window.getSelection()
-          var range = sel.rangeCount ? sel.getRangeAt(0) : null
-          var textBefore = ''
-          if (range) {
-            var preRange = document.createRange()
-            preRange.selectNodeContents(object)
-            preRange.setEnd(range.startContainer, range.startOffset)
-            textBefore = preRange.toString()
-          }
+    if (object.isContentEditable) {
+      var escaped = object.textContent.replace(/&/g, '\x26amp;').replace(/</g, '\x26lt;').replace(/>/g, '\x26gt;')
+      this._validate(object, escaped, object.textContent, language)
 
-          var before = object.textContent
-          var escaped = before.replace(/&/g, '\x26amp;').replace(/</g, '\x26lt;').replace(/>/g, '\x26gt;')
-          var lang = self._detectLanguage(escaped)
-          var colorized = escaped
+      if (!object._syntaxBound) {
+        object._syntaxBound = true
+        this.format(object)
+        var timer = null
 
-          if (lang === 'html') {
-            colorized = self._colorizeHtml(escaped, self.config.colors)
-          } else if (lang === 'javascript') {
-            colorized = self._colorizeJavaScript(escaped, self.config.javascriptColors || self.config.colors)
-          } else if (lang === 'shell') {
-            colorized = self._colorizeShell(escaped, self.config.shellColors || self.config.colors)
-          }
+        object.addEventListener('paste', function (e) {
+          e.preventDefault()
+          var text = (e.clipboardData || window.clipboardData).getData('text/plain')
+          document.execCommand('insertText', false, text)
+          setTimeout(function () { self.format(object) }, 0)
+        })
 
-          object.innerHTML = colorized
-
-          // Validate and highlight errors
-          self._validate(object, escaped, before, lang)
-
-          // Restore cursor
-          var walker = document.createTreeWalker(object, NodeFilter.SHOW_TEXT)
-          var charCount = 0, node
-          while (node = walker.nextNode()) {
-            var next = charCount + node.textContent.length
-            if (next >= textBefore.length) {
-              var r = document.createRange()
-              r.setStart(node, textBefore.length - charCount)
-              r.collapse(true)
-              sel.removeAllRanges()
-              sel.addRange(r)
-              return
+        object.addEventListener('input', function () {
+          clearTimeout(timer)
+          timer = setTimeout(function () {
+            var sel = window.getSelection()
+            var range = sel.rangeCount ? sel.getRangeAt(0) : null
+            var textBefore = ''
+            if (range) {
+              var preRange = document.createRange()
+              preRange.selectNodeContents(object)
+              preRange.setEnd(range.startContainer, range.startOffset)
+              textBefore = preRange.toString()
             }
-            charCount = next
-          }
-        }, 500)
-      })
+
+            var before = object.textContent
+            if (before === object._lastText) return
+            object._lastText = before
+            var escaped = before.replace(/&/g, '\x26amp;').replace(/</g, '\x26lt;').replace(/>/g, '\x26gt;')
+            var lang = self._detectLanguage(escaped)
+            var colorized = escaped
+
+            if (lang === 'html') {
+              colorized = self._colorizeHtml(escaped, self.config.colors)
+            } else if (lang === 'javascript') {
+              colorized = self._colorizeJavaScript(escaped, self.config.javascriptColors || self.config.colors)
+            } else if (lang === 'shell') {
+              colorized = self._colorizeShell(escaped, self.config.shellColors || self.config.colors)
+            }
+
+            object.innerHTML = colorized
+
+            // Validate and highlight errors
+            self._validate(object, escaped, before, lang)
+
+            // Restore cursor
+            var walker = document.createTreeWalker(object, NodeFilter.SHOW_TEXT)
+            var charCount = 0, node
+            while (node = walker.nextNode()) {
+              var next = charCount + node.textContent.length
+              if (next >= textBefore.length) {
+                var r = document.createRange()
+                r.setStart(node, textBefore.length - charCount)
+                r.collapse(true)
+                sel.removeAllRanges()
+                sel.addRange(r)
+                return
+              }
+              charCount = next
+            }
+          }, 500)
+        })
+      }
     }
+  },
+
+  format: function (object) {
+    if (object.exec) object = object.exec.element
+    var indent = this.config.indent || '  '
+    var text = object.textContent
+    var voidTags = /^<(br|hr|img|input|meta|link|area|base|col|embed|source|track|wbr)\b/
+
+    // Split adjacent tags onto separate lines (but keep <tag>text</tag> together)
+    text = text.replace(/>(<[a-zA-Z\/])/g, '>\n$1')
+
+    var lines = text.split('\n')
+    var level = 0
+    var formatted = []
+
+    for (var i = 0; i < lines.length; i++) {
+      var line = lines[i].trim()
+      if (!line) continue
+
+      // Decrease indent for closing tags
+      if (/^<\//.test(line)) level--
+
+      formatted.push(Array(Math.max(level, 0) + 1).join(indent) + line)
+
+      // Increase indent for opening tags (not self-closing, not void, not closing, not same-line close)
+      if (/^<[a-zA-Z]/.test(line) && !/\/>$/.test(line) && !voidTags.test(line) && !/^<\//.test(line)) {
+        var tagMatch = line.match(/^<([a-zA-Z][a-zA-Z0-9]*)/)
+        if (tagMatch && new RegExp('</' + tagMatch[1] + '>').test(line)) {
+          // Tag opens and closes on same line, don't increase
+        } else {
+          level++
+        }
+      }
+    }
+
+    object.textContent = formatted.join('\n')
+    this.set(object)
   },
 
   _validate: function (object, escaped, before, language) {
@@ -97,10 +151,28 @@ app.plugin.syntaxhighlighting = {
       if (dq % 2 !== 0 || sq % 2 !== 0) errorLines[i] = true
 
       if (language === 'html') {
-        // Invalid opening tag name (e.g. <div#>)
-        if (/&lt;[a-zA-Z]+[#!@$%^*~]+/.test(line)) errorLines[i] = true
-        // Invalid closing tag (e.g. </div#>)
-        if (/&lt;\/\w*[#!@$%^*~]+/.test(line)) errorLines[i] = true
+        var rawLine = object.textContent.split('\n')[i] || ''
+        // Invalid closing tag (e.g. </head#>)
+        if (/<\/[a-zA-Z]*[^a-zA-Z0-9>\s]+[^>]*>/.test(rawLine)) { errorLines[i] = true; console.error('Syntax error (line ' + (i + 1) + '): Invalid closing tag') }
+        // Invalid tag name (e.g. <h1#>)
+        if (/<[a-zA-Z][a-zA-Z0-9]*[^a-zA-Z0-9\s>"'=\/\-]/.test(rawLine)) { errorLines[i] = true; console.error('Syntax error (line ' + (i + 1) + '): Invalid tag name') }
+        // Invalid attribute name (e.g. <div #@id="">)
+        var rawNoValues = rawLine.replace(/"[^"]*"|'[^']*'/g, '')
+        if (/<[a-zA-Z][a-zA-Z0-9]*\s+[^>]*[^a-zA-Z0-9\s>"'=\/\-:_.]/.test(rawNoValues)) { errorLines[i] = true; console.error('Syntax error (line ' + (i + 1) + '): Invalid attribute name') }
+        // Unclosed tag - skip, multiline tags are valid
+        // Empty tag name (e.g. <> or </>)
+        if (/<\s*>|<\/\s*>/.test(rawLine)) { errorLines[i] = true; console.error('Syntax error (line ' + (i + 1) + '): Empty tag name') }
+        // Double opening brackets (e.g. <<div>)
+        if (/<</.test(rawLine)) { errorLines[i] = true; console.error('Syntax error (line ' + (i + 1) + '): Double opening bracket') }
+        // Attribute value without quotes containing spaces (e.g. <div class=my class>)
+        if (/<[a-zA-Z][^>]*=\s*[^\s"'>][^\s"'>]*\s+[a-zA-Z]/.test(rawLine)) { errorLines[i] = true; console.error('Syntax error (line ' + (i + 1) + '): Unquoted attribute value with spaces') }
+        // Closing tag with attributes (e.g. </div class="x">)
+        if (/<\/[a-zA-Z][a-zA-Z0-9]*\s+[^>]+>/.test(rawLine)) { errorLines[i] = true; console.error('Syntax error (line ' + (i + 1) + '): Closing tag with attributes') }
+        // Tag starting with number (e.g. <1div>)
+        if (/<[0-9]/.test(rawLine)) { errorLines[i] = true; console.error('Syntax error (line ' + (i + 1) + '): Tag name starts with number') }
+        // Duplicate equals in attribute (e.g. <div id=="test">)
+        if (/=[^"'>\s]*=/.test(rawLine)) { errorLines[i] = true; console.error('Syntax error (line ' + (i + 1) + '): Duplicate equals sign') }
+        // Stray closing bracket - skip, multiline tags are valid
       }
 
       if (language === 'javascript') {
@@ -129,7 +201,6 @@ app.plugin.syntaxhighlighting = {
       var outLines = object.innerHTML.split('\n')
       for (var i in errorLines) {
         if (outLines[i] !== undefined) {
-          console.error('Syntax error on line ' + (parseInt(i) + 1) + ':', rawLines[i])
           outLines[i] = '<mark style="display:inline;background:' + this.config.errorColor + '">' + outLines[i] + '</mark>'
         }
       }
