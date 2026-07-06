ee78a6c92aa356dedd7026ff8b0a8642aba05625
Josef Gabrielsson <josef.gabrielsson@ericsson.com>
2026-07-06 15:57:16 +0200

    Build 697

--- nightly/front.js
+++ nightly/front.js
@@ -2685,7 +2685,7 @@ var app = {
    * @desc Handles global variables for the application.
    */
   globals: {
-    frontVersion: { major: 1, minor: 0, patch: 0, build: 696 },
+    frontVersion: { major: 1, minor: 0, patch: 0, build: 697 },
     language: document.documentElement.lang || 'en',
     docMode: document.documentMode || 0,
     isFrontpage: document.doctype ? true : false,
