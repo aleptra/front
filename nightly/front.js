44468713fbca45276bc6f77be1b1a4730d93835a
Josef Gabrielsson <josef.gabrielsson@ericsson.com>
2026-07-07 00:08:34 +0200

    Build 700

--- nightly/front.js
+++ nightly/front.js
@@ -1648,7 +1648,7 @@ var app = {
     style.id = 'front-reset'
     style.textContent = '*, *::before, *::after { box-sizing: border-box } ' +
       'body, h1, h2, h3, h4, h5, h6, p, figure, blockquote, dl, dd, ul, ol { margin: 0 } ' +
-      'ul, ol { padding: 0 } ' +
+      'ul, ol { padding: 1rem 2rem } ' +
       'body { min-height: 100dvh; line-height: 1.5; -webkit-font-smoothing: antialiased } ' +
       'img, picture, video, canvas, svg { display: block; max-width: 100% } ' +
       'input, button, textarea, select { font: inherit } ' +
@@ -2688,7 +2688,7 @@ var app = {
    * @desc Handles global variables for the application.
    */
   globals: {
-    frontVersion: { major: 1, minor: 0, patch: 0, build: 699 },
+    frontVersion: { major: 1, minor: 0, patch: 0, build: 700 },
     language: document.documentElement.lang || 'en',
     docMode: document.documentMode || 0,
     isFrontpage: document.doctype ? true : false,
