adeb15a9eb0f36fd266823389ae67dc5b7b94372
Josef Gabrielsson <josef.gabrielsson@ericsson.com>
2026-08-10 16:01:39 +0200

    Build 734

--- nightly/front.js
+++ nightly/front.js
@@ -249,7 +249,7 @@ var dom = {
       to = element.call.element
 
     if (!from || !to) return
-
+    from.id = '' // clear id
     to.innerHTML = to.getAttribute('inherit') === 'false' ? from.innerHTML : ''
     if (to.getAttribute('inherit') !== 'false') to.appendChild(from.cloneNode(true))
   },
@@ -2750,7 +2750,7 @@ var app = {
    * @desc Handles global variables for the application.
    */
   globals: {
-    frontVersion: { major: 1, minor: 0, patch: 0, build: 733 },
+    frontVersion: { major: 1, minor: 0, patch: 0, build: 734 },
     language: document.documentElement.lang || 'en',
     docMode: document.documentMode || 0,
     isFrontpage: document.doctype ? true : false,
