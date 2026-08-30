8e4347215205bc2fc8a6ccdf8df0d8a3579b66d0
Josef Gabrielsson <josef.gabrielsson@ericsson.com>
2026-08-30 20:04:05 +0200

    Build 762

--- src/front.js
+++ src/front.js
@@ -2809,7 +2809,7 @@ var app = previousApp || {
    * @desc Handles global variables for the application.
    */
   globals: {
-    frontVersion: { major: 1, minor: 1, patch: 0, build: 761 },
+    frontVersion: { major: 1, minor: 1, patch: 0, build: 762 },
     language: document.documentElement.lang || 'en',
     docMode: document.documentMode || 0,
     isFrontpage: document.doctype ? true : false,
