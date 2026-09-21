fe2a5298743b049ebbbe32ddcb4c3b6f200a4331
Josef Gabrielsson <josef.gabrielsson@ericsson.com>
2026-09-21 15:42:34 +0200

    Build 808

--- src/front.js
+++ src/front.js
@@ -1728,8 +1728,7 @@ var dom = {
   }
 }
 
-var previousApp = typeof window !== 'undefined' && window.app,
-  isLocalNetwork = location.protocol === 'file:' || /localhost|127\.0\.0\.1|::1|\.local/i.test(location.hostname)
+var previousApp = typeof window !== 'undefined' && window.app
 
 var app = previousApp || {
   module: previousApp && previousApp.module || {},
@@ -1739,7 +1738,7 @@ var app = previousApp || {
   docMode: document.documentMode || 0,
   srcDocTemplate: '',
   srcTemplate: [],
-  isLocalNetwork: isLocalNetwork,
+  isLocalNetwork: this.isLocalNetwork,
   spa: false,
   vars: { total: 0, totalStore: 0, loaded: 0 },
   modules: { total: 0, loaded: 0 },
@@ -1780,6 +1779,8 @@ var app = previousApp || {
         element = app.element.select(selector)
 
       app.disable(true)
+      app.isLocalNetwork = location.protocol === 'file:' || /localhost|127\.0\.0\.1|::1|\.local/i.test(location.hostname)
+      app.globals.set('isLocalNetwork', app.isLocalNetwork)
 
       // Load config file
       if (app.isLocalNetwork) {
@@ -2905,11 +2906,11 @@ var app = previousApp || {
    * @desc Handles global variables for the application.
    */
   globals: {
-    frontVersion: { major: 1, minor: 1, patch: 0, build: 807 },
+    frontVersion: { major: 1, minor: 1, patch: 0, build: 808 },
     language: document.documentElement.lang || 'en',
     docMode: document.documentMode || 0,
     isFrontpage: document.doctype ? true : false,
-    isLocalNetwork: this.isLocalNetwork,
+    isLocalNetwork: false,
     href: '',
     title: '',
     windowHeight: (window.visualViewport && window.visualViewport.height) || window.innerHeight,
